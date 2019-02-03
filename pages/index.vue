79237ea7b087b4e1fea535b5f5423f119522f1ae
corocn <crsk.plmzh@gmail.com>
2019-02-03 21:02:17 +0900

    initialized with nuxt + typescript

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <div>
+      <Logo />
+      <HelloWorld />
+    </div>
+  </section>
+</template>
+
+<script>
+  import Logo from '~/components/Logo.vue'
+  import HelloWorld from '~/components/HelloWorld.vue'
+
+  export default {
+    components: {
+      Logo,
+      HelloWorld
+    }
+  }
+</script>
