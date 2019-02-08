7650043b0542bb312de9ef796cd8b6112c2e7b72
corocn <crsk.plmzh@gmail.com>
2019-02-08 23:12:43 +0900

    faviconつくった

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'Slack Emoji Generator - Slack絵文字ジェネレーター',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
