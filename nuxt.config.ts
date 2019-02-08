d44ed69575773d185173e237ce6b1bb343defa0e
corocn <crsk.plmzh@gmail.com>
2019-02-09 00:13:26 +0900

    SPAモード & パッケージ更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 const pkg = require('./package')
 
 module.exports = {
-  mode: 'universal',
+  mode: 'spa',
 
   /*
    ** Headers of the page
@@ -34,8 +34,6 @@ module.exports = {
       ]
     }
   },
-
-
   /*
    ** Customize the progress-bar color
    */
