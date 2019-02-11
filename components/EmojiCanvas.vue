e4bc7a6ee5cbe7a5075a3de9b16f095d5e8636ae
corocn <crsk.plmzh@gmail.com>
2019-02-11 21:58:45 +0900

    描画改善

--- components/EmojiCanvas.vue
+++ components/EmojiCanvas.vue
@@ -43,48 +43,46 @@ export default class EmojiCanvas extends Vue {
   mounted(): void {
     const el = this.$el as HTMLCanvasElement
     this.ctx = el.getContext('2d')
-
-    const font = new FontFaceObserver(this.family, { weight: this.weight })
-
-    font.load().then(() => {
-      this.draw()
-    })
+    this.draw()
   }
 
   draw(): void {
-    if (this.ctx) {
-      this.ctx.fillStyle = `rgba(${this.rgba.r}, ${this.rgba.g}, ${
-        this.rgba.b
-      }, ${this.rgba.a})`
-      this.ctx.textAlign = 'left'
-      this.ctx.textBaseline = 'top'
-      this.ctx.font = `${this.weight} ${this.size}px '${this.family}'`
-      this.ctx.beginPath()
-      this.ctx.clearRect(0, 0, 200, 200)
+    const font = new FontFaceObserver(this.family, { weight: this.weight })
+    font.load(this.text).then(() => {
+      if (this.ctx) {
+        this.ctx.fillStyle = `rgba(${this.rgba.r}, ${this.rgba.g}, ${
+          this.rgba.b
+        }, ${this.rgba.a})`
+        this.ctx.textAlign = 'left'
+        this.ctx.textBaseline = 'top'
+        this.ctx.font = `${this.weight} ${this.size}px '${this.family}'`
+        this.ctx.beginPath()
+        this.ctx.clearRect(0, 0, 200, 200)
 
-      const lines: Array<string> = this.text.split('\n')
+        const lines: Array<string> = this.text.split('\n')
 
-      if (lines.length === 1 && lines[0].length > 1) {
-        // 1行だけの場合は、縦に引き伸ばす
-        this.ctx.scale(1, 2)
-      } else if (
-        lines.length > 1 &&
-        lines[0].length === 1 &&
-        lines[1].length === 1
-      ) {
-        // 1列だけの場合は、横に引き伸ばす
-        this.ctx.scale(2, 1)
-      }
-
-      lines.map((value: string, index) => {
-        if (this.ctx) {
-          this.ctx.fillText(value, 0, index * 100, 200)
+        if (lines.length === 1 && lines[0].length > 1) {
+          // 1行だけの場合は、縦に引き伸ばす
+          this.ctx.scale(1, 2)
+        } else if (
+          lines.length > 1 &&
+          lines[0].length === 1 &&
+          lines[1].length === 1
+        ) {
+          // 1列だけの場合は、横に引き伸ばす
+          this.ctx.scale(2, 1)
         }
-      })
 
-      // reset scale
-      this.ctx.setTransform(1, 0, 0, 1, 0, 0)
-    }
+        lines.map((value: string, index) => {
+          if (this.ctx) {
+            this.ctx.fillText(value, 0, index * 100, 200)
+          }
+        })
+
+        // reset scale
+        this.ctx.setTransform(1, 0, 0, 1, 0, 0)
+      }
+    })
   }
 
   get rgba(): ColorFormats.RGBA {
