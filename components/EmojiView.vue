36d1fbe7f6151bffebde747011b5d49dddaf52b1
corocn <crsk.plmzh@gmail.com>
2019-08-11 18:19:25 +0900

    slack size

--- components/EmojiView.vue
+++ components/EmojiView.vue
@@ -26,7 +26,7 @@
         :family="family"
         :color="color"
         :weight="weight"
-        :view-size="64"
+        :view-size="40"
       />
       <emoji-canvas
         class="s16 mx-1"
@@ -35,7 +35,7 @@
         :family="family"
         :color="color"
         :weight="weight"
-        :view-size="32"
+        :view-size="20"
       />
     </div>
   </div>
